59f4c2e0a008ff961a35ee6b128f90c6454ce23b
phatnguyenuit <PhatNguyen@PHATNGUYEN>
2017-07-21 01:55:35 +0700

    add document for matrix in c++

--- HocLapTrinhC++/Sophuc.cpp
+++ HocLapTrinhC++/Sophuc.cpp
@@ -1,137 +1,137 @@
-#include <stdio.h>
-struct sophuc {
-	float phanthuc, phanao;
-};
-void nhap( sophuc &x ) {
-	printf( "Nhap vao phan thuc cua so phuc : " );
-	scanf_s( "%f", &x.phanthuc );
-	printf( "Nhap vao phan ao cua so phuc : " );
-	scanf_s( "%f", &x.phanao );
-}
-sophuc cong2sophuc( sophuc &x, sophuc &y ) {
-	sophuc tong;
-	tong.phanthuc = x.phanthuc + y.phanthuc;
-	tong.phanao = x.phanao + y.phanao;
-	return tong;
-}
-sophuc tru2sophuc( sophuc &x, sophuc &y ) {
-	sophuc hieu;
-	hieu.phanthuc = x.phanthuc - y.phanthuc;
-	hieu.phanao = x.phanao - y.phanao;
-	return hieu;
-}
-sophuc nhan2sophuc( sophuc &x, sophuc &y ) {
-	sophuc tich;
-	tich.phanthuc = x.phanthuc * y.phanthuc - x.phanao * y.phanao;
-	tich.phanao = x.phanthuc * y.phanao + x.phanao * y.phanthuc;
-	return tich;
-}
-sophuc chia2sophuc( sophuc &x, sophuc &y ) {
-	sophuc tich, thuong, ylienhop;
-	if (y.phanthuc*y.phanao != 0) {
-		ylienhop.phanthuc = y.phanthuc;
-		ylienhop.phanao = -y.phanao;
-		tich = nhan2sophuc( x, ylienhop );
-		float a = y.phanthuc * y.phanthuc + y.phanao * y.phanao;
-		thuong.phanthuc = tich.phanthuc / a;
-		thuong.phanao = tich.phanao / a;
-	}
-	else
-	{
-		thuong.phanthuc = 0;
-		thuong.phanao = 0;
-	}
-	return thuong;
-}
-void xuat( sophuc &x ) {
-	printf( "So phuc %f + %fi \n", x.phanthuc, x.phanao );
-}
-void main() {
-	sophuc so1, so2, tong, hieu, tich, thuong;
-	int luachon;
-	bool danhap = false;
-	do
-	{
-		printf( "|------------------------------------------|\n" );
-		printf( "|--1.Nhap 2 so phuc                        |\n" );
-		printf( "|--2.Cong 2 so phuc                        |\n" );
-		printf( "|--3.Tru 2 so phuc                         |\n" );
-		printf( "|--4.Nhan 2 so phuc                        |\n" );
-		printf( "|--5.Chia 2 so phuc                        |\n" );
-		printf( "|--6.Thoat                                 |\n" );
-		printf( "|------------------------------------------|\n" );
-		printf( "---->Nhap vao lua chon cua ban :  " );
-		scanf_s( "%d", &luachon );
-		printf( "\n" );
-		switch (luachon)
-		{
-		case 1: {
-			printf( "1.Nhap 2 so phuc \n" );
-			nhap( so1 );
-			xuat( so1 );
-			nhap( so2 );
-			xuat( so2 );
-			danhap = true;
-			break;
-		}
-		case 2: {
-			if (danhap == true)
-			{
-				printf( "2.Cong 2 so phuc " );
-				tong = cong2sophuc( so1, so2 );
-				printf( "\nTong cua 2 so phuc la : " );
-				xuat( tong );
-			}
-			else
-				printf( "\nBan chua nhap 2 so phuc \n" );
-			break;
-		}
-		case 3: {
-			if (danhap == true) {
-				printf( "3.Tru 2 so phuc \n" );
-				hieu = tru2sophuc( so1, so2 );
-				printf( "\nHieu cua 2 so phuc la : " );
-				xuat( hieu );
-			}
-			else
-				printf( "\nBan chua nhap 2 so phuc \n" );
-			break;
-		}
-		case 4: {
-			if (danhap == true) {
-				printf( "4.Nhan 2 so phuc \n" );
-				tich = nhan2sophuc( so1, so2 );
-				printf( "\nTich cua 2 so phuc la : " );
-				xuat( tich );
-			}
-			else
-				printf( "\nBan chua nhap 2 so phuc \n" );
-			break;
-		}
-		case 5: {
-			if (danhap == true) {
-				printf( "5.Chia 2 so phuc \n" );
-				thuong = chia2sophuc( so1, so2 );
-				if (thuong.phanthuc * thuong.phanao != 0) {
-					printf( "\nThuong cua 2 so phuc la : " );
-					xuat( thuong );
-				}
-				else
-
-					printf( "Loi! Khong chia duoc " );
-			}
-			else
-				printf( "\nBan chua nhap 2 so phuc \n" );
-			break;
-		}
-		case 6: {
-			printf( "6.Thoat \n" );
-			break;
-		}
-		default: {
-			printf( "Nhap lai lua chon cua ban \n" );
-			break;
-		}
-		}
-	} while (luachon != 6);
-}
+//#include <stdio.h>
+//struct sophuc {
+//	float phanthuc, phanao;
+//};
+//void nhap( sophuc &x ) {
+//	printf( "Nhap vao phan thuc cua so phuc : " );
+//	scanf_s( "%f", &x.phanthuc );
+//	printf( "Nhap vao phan ao cua so phuc : " );
+//	scanf_s( "%f", &x.phanao );
+//}
+//sophuc cong2sophuc( sophuc &x, sophuc &y ) {
+//	sophuc tong;
+//	tong.phanthuc = x.phanthuc + y.phanthuc;
+//	tong.phanao = x.phanao + y.phanao;
+//	return tong;
+//}
+//sophuc tru2sophuc( sophuc &x, sophuc &y ) {
+//	sophuc hieu;
+//	hieu.phanthuc = x.phanthuc - y.phanthuc;
+//	hieu.phanao = x.phanao - y.phanao;
+//	return hieu;
+//}
+//sophuc nhan2sophuc( sophuc &x, sophuc &y ) {
+//	sophuc tich;
+//	tich.phanthuc = x.phanthuc * y.phanthuc - x.phanao * y.phanao;
+//	tich.phanao = x.phanthuc * y.phanao + x.phanao * y.phanthuc;
+//	return tich;
+//}
+//sophuc chia2sophuc( sophuc &x, sophuc &y ) {
+//	sophuc tich, thuong, ylienhop;
+//	if (y.phanthuc*y.phanao != 0) {
+//		ylienhop.phanthuc = y.phanthuc;
+//		ylienhop.phanao = -y.phanao;
+//		tich = nhan2sophuc( x, ylienhop );
+//		float a = y.phanthuc * y.phanthuc + y.phanao * y.phanao;
+//		thuong.phanthuc = tich.phanthuc / a;
+//		thuong.phanao = tich.phanao / a;
+//	}
+//	else
+//	{
+//		thuong.phanthuc = 0;
+//		thuong.phanao = 0;
+//	}
+//	return thuong;
+//}
+//void xuat( sophuc &x ) {
+//	printf( "So phuc %f + %fi \n", x.phanthuc, x.phanao );
+//}
+//void main() {
+//	sophuc so1, so2, tong, hieu, tich, thuong;
+//	int luachon;
+//	bool danhap = false;
+//	do
+//	{
+//		printf( "|------------------------------------------|\n" );
+//		printf( "|--1.Nhap 2 so phuc                        |\n" );
+//		printf( "|--2.Cong 2 so phuc                        |\n" );
+//		printf( "|--3.Tru 2 so phuc                         |\n" );
+//		printf( "|--4.Nhan 2 so phuc                        |\n" );
+//		printf( "|--5.Chia 2 so phuc                        |\n" );
+//		printf( "|--6.Thoat                                 |\n" );
+//		printf( "|------------------------------------------|\n" );
+//		printf( "---->Nhap vao lua chon cua ban :  " );
+//		scanf_s( "%d", &luachon );
+//		printf( "\n" );
+//		switch (luachon)
+//		{
+//		case 1: {
+//			printf( "1.Nhap 2 so phuc \n" );
+//			nhap( so1 );
+//			xuat( so1 );
+//			nhap( so2 );
+//			xuat( so2 );
+//			danhap = true;
+//			break;
+//		}
+//		case 2: {
+//			if (danhap == true)
+//			{
+//				printf( "2.Cong 2 so phuc " );
+//				tong = cong2sophuc( so1, so2 );
+//				printf( "\nTong cua 2 so phuc la : " );
+//				xuat( tong );
+//			}
+//			else
+//				printf( "\nBan chua nhap 2 so phuc \n" );
+//			break;
+//		}
+//		case 3: {
+//			if (danhap == true) {
+//				printf( "3.Tru 2 so phuc \n" );
+//				hieu = tru2sophuc( so1, so2 );
+//				printf( "\nHieu cua 2 so phuc la : " );
+//				xuat( hieu );
+//			}
+//			else
+//				printf( "\nBan chua nhap 2 so phuc \n" );
+//			break;
+//		}
+//		case 4: {
+//			if (danhap == true) {
+//				printf( "4.Nhan 2 so phuc \n" );
+//				tich = nhan2sophuc( so1, so2 );
+//				printf( "\nTich cua 2 so phuc la : " );
+//				xuat( tich );
+//			}
+//			else
+//				printf( "\nBan chua nhap 2 so phuc \n" );
+//			break;
+//		}
+//		case 5: {
+//			if (danhap == true) {
+//				printf( "5.Chia 2 so phuc \n" );
+//				thuong = chia2sophuc( so1, so2 );
+//				if (thuong.phanthuc * thuong.phanao != 0) {
+//					printf( "\nThuong cua 2 so phuc la : " );
+//					xuat( thuong );
+//				}
+//				else
+//
+//					printf( "Loi! Khong chia duoc " );
+//			}
+//			else
+//				printf( "\nBan chua nhap 2 so phuc \n" );
+//			break;
+//		}
+//		case 6: {
+//			printf( "6.Thoat \n" );
+//			break;
+//		}
+//		default: {
+//			printf( "Nhap lai lua chon cua ban \n" );
+//			break;
+//		}
+//		}
+//	} while (luachon != 6);
+//}
---
+++ HocLapTrinhC++/mang-2-chieu.cpp
@@ -0,0 +1,86 @@
+﻿#pragma region Mảng 2 chiều và cách sử dụng
+/*
+	Part 1 : Khai báo mảng 2 chiều :
+		Có 2 cách khai báo mảng 2 chiều ( ma trận ) :
+		-Tường minh : <kiểu dữ liệu> <tên mảng>[số dòng][số cột]
+			vd :	int mang[2][2] ; // Ma trận 2 dòng 2 cột với từng phần tử mang kiểu int
+			float mang[2][4] ; // Ma trận 2 dòng 2 cột với từng phần tử mang kiểu float
+		- Không tường minh :
+			Bước 1 : typedef <kiểu dữ liệu> <tên thay thế>[số dòng][số cột];
+			Bước 2 : <từ khoá thay thế> mt1; // Nghĩa là khai báo 1 mảng 2 chiều tên là mt1 với kiểu dữ liệu là <tên thay thế>
+			vd :	typedef int MATRIX[2][2];
+					MATRIX mt1;
+	Part 2 : Truy xuất phần tử và duyệt mảng 2 chiều
+		-Truy xuất
+		Chỉ cần gọi <tên mảng 2 chiều>[chỉ số dòng][chỉ số cột] là truy xuất được phần tử ở vị trí dòng <chỉ số dòng> và cột <chỉ số cột>
+		vd : int value = mang[0][1]; // truy xuất phần tử ở dòng 0 cột 1 
+		NOTE : chỉ số dòng và cột đều bắt đầu từ 0
+		-Duyệt mảng 2 chiều : Chỉ cần duyệt lần lượt từng dòng và từng cột tương ứng với dòng đang duyệt
+			for(i in <số dòng>){
+				for( j in <số cột> ){
+					//thao tác
+				}
+			}
+		vd : 
+		Nhập mảng 2 chiều
+		for (int i = 0; i < sodong; i++) {
+			for (int j = 0; j < socot; j++) {
+				mang[i][j] = 100; // gán giá trị cho phần tử thứ ij bằng 100;
+			}
+		}
+		Xuất mảng 2 chiều
+		for (int i = 0; i < sodong; i++) {
+			for (int j = 0; j < socot; j++) {
+				//thao tác xuất
+			}
+		}
+
+
+*/
+
+#pragma endregion
+
+//nên dùng từ khoá const để khai báo hằng số để xác định rõ ràng kiểu dữ liệu của hằng số
+const int sodong = 2;
+const int socot = 2;
+#include <iostream>
+using namespace std;
+
+void main() {
+	//khai báo tường minh
+	int matran[sodong][socot];
+
+	//khai báo không tường minh
+	typedef int matrix[sodong][socot];
+	matrix mt;
+
+	//nhập ma trận matran
+	for (int i = 0; i < sodong; i++) {
+		for (int j = 0; j < socot; j++) {
+			matran[i][j] = 100; // gán giá trị cho phần tử thứ ij bằng 100;
+		}
+	}
+
+	//nhập ma trận mt 
+	for (int i = 0; i < sodong; i++) {
+		for (int j = 0; j < socot; j++) {
+			mt[i][j] = 200; // gán giá trị cho phần tử thứ ij bằng 100;
+		}
+	}
+
+	cout << "=======================================================" << endl;
+	cout << "Xuat mang 2 chieu khai bao tuong minh matran[" << sodong << " ][ " << socot << " ]" << endl;
+	for (int i = 0; i < sodong; i++) {
+		for (int j = 0; j < socot; j++) {
+			cout << "matran[ " << i << " ][ " << j << " ] : " << matran[i][j] << endl;
+		}
+	}
+	cout << "=======================================================" << endl;
+	cout << "Xuat mang 2 chieu khai bao tuong minh mt" << endl;
+	for (int i = 0; i < sodong; i++) {
+		for (int j = 0; j < socot; j++) {
+			cout << "mt[ " << i << " ][ " << j << " ] : " << mt[i][j] << endl;
+		}
+	}
+	cout << "=======================================================" << endl;
+}
