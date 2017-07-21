bb3644245889eb43806e1b284a76b63814b0ffb8
phatnguyenuit <PhatNguyen@PHATNGUYEN>
2017-07-21 09:51:24 +0700

    update file mang-2-chieu.cpp

--- HocLapTrinhC++/mang-2-chieu.cpp
+++ HocLapTrinhC++/mang-2-chieu.cpp
@@ -1,15 +1,35 @@
 ﻿#pragma region Mảng 2 chiều và cách sử dụng
 /*
+	Part 0 : Khái niệm về mảng 2 chiều ( đa chiều );
+		Với mảng 1 chiều ta có mang = {phần tử 1, phần tử 2,....,phần tử n} với mỗi phần tử trong mảng mang giá trị
+		thuộc các kiểu dữ liệu cơ bản của C++ như int, float, double...
+		Tương tự với ý niệm của mảng 1 chiều ta có :
+			Mảng 2 chiều có dạng mang = {phần tử 1, phần tử 2,....,phần tử n} với mỗi phần tử trong mảng là 1 mảng 1 chiều
+			vd : mang = { {0,1,2}, {3,4,5}, {6,7,8} };
+			=> mang[0] = {0,1,2};
+			=> mang[0][0] = 0;
 	Part 1 : Khai báo mảng 2 chiều :
-		Có 2 cách khai báo mảng 2 chiều ( ma trận ) :
+		Có 3 cách khai báo mảng 2 chiều ( ma trận ) :
 		-Tường minh : <kiểu dữ liệu> <tên mảng>[số dòng][số cột]
 			vd :	int mang[2][2] ; // Ma trận 2 dòng 2 cột với từng phần tử mang kiểu int
 			float mang[2][4] ; // Ma trận 2 dòng 2 cột với từng phần tử mang kiểu float
 		- Không tường minh :
-			Bước 1 : typedef <kiểu dữ liệu> <tên thay thế>[số dòng][số cột];
-			Bước 2 : <từ khoá thay thế> mt1; // Nghĩa là khai báo 1 mảng 2 chiều tên là mt1 với kiểu dữ liệu là <tên thay thế>
-			vd :	typedef int MATRIX[2][2];
-					MATRIX mt1;
+			+Dùng từ khoá typedef
+				Bước 1 : typedef <kiểu dữ liệu> <tên thay thế>[số dòng][số cột];
+				Bước 2 : <từ khoá thay thế> mt1; // Nghĩa là khai báo 1 mảng 2 chiều tên là mt1 với kiểu dữ liệu là <tên thay thế>
+				vd :	typedef <kiểu ữ liệu> MATRIX[2][2];
+						MATRIX mt1;
+			+Dùng con trỏ 1 chiều và 2 chiều khi biết số dòng hoặc không biết cả 2
+				a) Khi biết số dòng của mảng 2 chiều
+					khai báo : <kiểu dữ liệu> *p[sodong]; //mảng con trỏ 1 chiều
+					khởi tạo : for(i in sodong)
+									p[i] = new int[socot];
+				b) Khi không có số dòng và số cột của mảng 2 chiều
+					khai báo : <kiểu dữ liệu> **p; //con trỏ 2 chiều
+					khởi tạo : 
+								p = new <kiểu dữ liệu> [sodong];
+								for(i in sodong)
+									p[i] = new int[socot];
 	Part 2 : Truy xuất phần tử và duyệt mảng 2 chiều
 		-Truy xuất
 		Chỉ cần gọi <tên mảng 2 chiều>[chỉ số dòng][chỉ số cột] là truy xuất được phần tử ở vị trí dòng <chỉ số dòng> và cột <chỉ số cột>
@@ -53,6 +73,21 @@ void main() {
 	//khai báo không tường minh
 	typedef int matrix[sodong][socot];
 	matrix mt;
+
+	//khai báo dạng con trỏ
+	int *p[sodong]; //mảng con trỏ 1 chiều 
+	int **max; // con trỏ 2 chiều
+
+	//Khởi tạo mảng 2 chiều bằng mảng con trỏ 1 chiều
+	for (int i = 0; i < sodong; i++)
+		p[i] = new int[socot];
+
+	//khởi tạo mảng 2 chiều bằng con trỏ 2 chiều
+	max = new int* [sodong];
+	for (int i = 0; i < sodong; i++)
+		max[i] = new int[socot];
+
+	//cách sử dụng mảng 2 chiều để nhập xuất tương tự với kiểu khai báo mảng 2 chiều tường minh
 
 	//nhập ma trận matran
 	for (int i = 0; i < sodong; i++) {
